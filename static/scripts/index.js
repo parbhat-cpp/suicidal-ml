9cd56b61ed71a178880940008d897e726bce1496
Parbhat Sharma <prabhat29ps@gmail.com>
2025-07-08 08:55:19 +0530

    prediction output

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -4,11 +4,15 @@ const predictionOutput = document.getElementById('prediction-output');
 const predictBtn = document.getElementById('predict-btn');
 
 predictBtn.addEventListener('click', async function () {
-    const userTextInput = document.getElementById('text').value;
+    const userTextInput = document.getElementById('text').value.trim();
 
     if (!userTextInput) {
         return;
     }
+
+    predictionOutput.innerHTML = `
+        <div class="loader"></div>
+    `;
 
     const predictionResponse = await fetch(`${BASE_URL}/predict-mental-health`,
         {
@@ -25,8 +29,34 @@ predictBtn.addEventListener('click', async function () {
     if (predictionResponse.ok) {
         const predictionData = await predictionResponse.json();
 
-        alert(predictionData);
+        const prediction = predictionData['prediction'];
+
+        if (prediction === 0.0) {
+            predictionOutput.innerHTML = `
+                <div class="prediction">
+                    <h1>
+                        No signs of suicidal ideation detected.
+                    </h1>
+                    <p>
+                        This is an AI-based prediction. If you're in distress, please reach out to a helpline or
+                        professional support.
+                    </p>
+                </div>
+            `;
+        } else {
+            predictionOutput.innerHTML = `
+                <div class="prediction">
+                    <h1>
+                        Warning: Signs of suicidal ideation detected in the text
+                    </h1>
+                    <p>
+                        This is an AI-based prediction. If you're in distress, please reach out to a helpline or
+                        professional support.
+                    </p>
+                </div>
+            `;
+        }
     } else {
-        alert('failed');
+        alert('Prediction failed! Please try again');
     }
 });
