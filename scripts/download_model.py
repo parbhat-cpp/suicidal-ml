369bfef41d42d0dcb216dc0d43e3ed4dd3380778
Parbhat Sharma <prabhat29ps@gmail.com>
2025-07-08 13:11:53 +0530

    deployment github actions

---
+++ scripts/download_model.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import kaggle
+
+from src.exception import CustomException
+
+def download_model_from_kaggle():
+    username = os.getenv('KAGGLE_USERNAME')
+    
+    os.makedirs('./models', exist_ok=True)
+    
+    try:
+        kaggle.api.dataset_download_files(
+            f"{username}/suicide-detection-model",
+            path="./models",
+            unzip=True,
+        )
+        
+        for root, dirs, files in os.walk('./models'):
+            for file in files:
+                print(f"Downloaded file: {os.path.join(root, file)}")
+    except Exception as e:
+        raise CustomException(e, sys)
