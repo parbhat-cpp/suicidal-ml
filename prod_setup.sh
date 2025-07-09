e53e5098afda4e5b00fcef3207469230aa5e1eaa
Parbhat Sharma <prabhat29ps@gmail.com>
2025-07-09 19:46:34 +0530

    update prod_setup.sh

--- prod_setup.sh
+++ prod_setup.sh
@@ -4,4 +4,10 @@ python scripts/download_model.py
 
 echo "Downloading NLTK resources..."
 
-python -c "import nltk; nltk.download('stopwords', download_dir='/opt/render/nltk_data'); nltk.download('punkt', download_dir='/opt/render/nltk_data'); nltk.download('wordnet', download_dir='/opt/render/nltk_data'); nltk.download('omw-1.4', download_dir='/opt/render/nltk_data')"
+python -c "
+import nltk
+nltk.download('stopwords', download_dir='/opt/render/nltk_data')
+nltk.download('punkt', download_dir='/opt/render/nltk_data')
+nltk.download('wordnet', download_dir='/opt/render/nltk_data')
+nltk.download('omw-1.4', download_dir='/opt/render/nltk_data')
+"
