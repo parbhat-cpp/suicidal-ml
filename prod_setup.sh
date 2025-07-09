bffe26677f3653a43524f736ef88f02ef39b221d
Parbhat Sharma <prabhat29ps@gmail.com>
2025-07-09 19:24:04 +0530

    update nltk data location

--- prod_setup.sh
+++ prod_setup.sh
@@ -4,4 +4,4 @@ python scripts/download_model.py
 
 echo "Downloading NLTK resources..."
 
-python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt'); nltk.download('wordnet'); nltk.download('omw-1.4')"
+python -c "import nltk; nltk.download('stopwords', download_dir='/opt/render/nltk_data'); nltk.download('punkt', download_dir='/opt/render/nltk_data'); nltk.download('wordnet', download_dir='/opt/render/nltk_data'); nltk.download('omw-1.4', download_dir='/opt/render/nltk_data')"
